7d147744bc4cf8551bf4944998b9e16db9477c84
Sakura55Blossom <66422123+Sakura55Blossom@users.noreply.github.com>
2021-08-29 11:11:06 +0100

    added completed jobs and customers listview pages

--- jobslist/urls.py
+++ jobslist/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
   path("", views.home, name="home"),
-  path("outstanding_jobs/", views.JobsList.as_view(), name="jobs_list")
+  path("outstanding_jobs/", views.JobsList.as_view(), name="jobs_list"),
+  path("completed_jobs/", views.CompletedJobsList.as_view(), name="completed_jobs_list"),
+  path("customers/", views.CustomerList.as_view(), name="customer_list"),
 ]
--- jobslist/views.py
+++ jobslist/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from .models import Customer, Job
 from django.views.generic import ListView
 
-class JobsList(ListView):
-  model = Job
-  template_name = "jobslist/outstanding_jobs.html"
-
 # Create your views here.
 def home(request):
     return render(request, "jobslist/home.html")
+
+class JobsList(ListView):
+    model = Job
+    template_name = "jobslist/outstanding_jobs.html"
+
+class CompletedJobsList(ListView):
+    model = Job
+    template_name = "jobslist/completed_jobs.html"
+
+class CustomerList(ListView):
+    model = Customer
+    template_name = "jobslist/customers.html"
