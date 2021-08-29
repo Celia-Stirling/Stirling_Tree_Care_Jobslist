601251e6eff0bff2d56bab6999c393e45f729cc4
Sakura55Blossom <66422123+Sakura55Blossom@users.noreply.github.com>
2021-08-29 08:46:52 +0100

    created outstanding jobs page

--- jobslist/urls.py
+++ jobslist/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
   path("", views.home, name="home"),
+  path("outstanding_jobs/", views.JobsList.as_view(), name="jobs_list")
 ]
--- jobslist/views.py
+++ jobslist/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Customer, Job
+from django.views.generic import ListView
+
+class JobsList(ListView):
+  model = Job
+  template_name = "jobslist/outstanding_jobs.html"
 
 # Create your views here.
 def home(request):
