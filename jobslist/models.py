2d020a7176138a7833be2dbccba6da3add83b042
Sakura55Blossom <66422123+Sakura55Blossom@users.noreply.github.com>
2021-10-05 21:55:42 +0100

    made it so that email field could be blank

--- jobslist/models.py
+++ jobslist/models.py
@@ -9,7 +9,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     phone = models.CharField(max_length=30)
-    email = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254, null=True, blank=True)
     def __str__(self):
         return self.last_name + ", " + self.first_name
     def get_absolute_url(self):
